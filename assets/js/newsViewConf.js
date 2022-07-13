ff92a116cf39a5d9bae93d18c947cf06133a5c1c
LEVINSKY Nicolas <nicolasdavidlevinsky@gmail.com>
2022-07-13 17:52:53 -0300

    JS y Eco Notes Newsv1

---
+++ assets/js/newsViewConf.js
@@ -0,0 +1,23 @@
+
+let URLsearch = window.location.search.substring(4,6)
+const tituloNew = document.getElementById("titleNew")
+const fechaNew = document.getElementById("fechaNew")
+const bannerNew = document.getElementById("fotoContent")
+const bodyNew = document.getElementById("bodyNew")
+
+console.log(noticias[idNew].titulo)
+
+const cargaContenido = () =>{
+    tituloNew.textContent = "hola"
+}
+
+//Meta
+document.querySelector('meta[name="description"]').setAttribute("content",noticias[URLsearch].metaDes );
+document.querySelector('meta[name="keywords"]').setAttribute("content",noticias[URLsearch].metaKeys );
+
+tituloNew.textContent = noticias.reverse()[URLsearch].titulo
+fechaNew.textContent = noticias[URLsearch].fecha
+bannerNew.innerHTML = `<img id="bannerNew" src="${noticias[URLsearch].img}" alt="">`
+bodyNew.textContent = noticias[URLsearch].cuerpo
+
+
